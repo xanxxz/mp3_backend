fd93c145edfcea9e2b975f070d7d6f170f315575
xanxxz <boneislandman00@gmail.com>
2026-04-23 18:32:22 +0400

    check

--- src/server.ts
+++ src/server.ts
@@ -346,8 +346,8 @@ app.get('/orders', { preValidation: [(app as any).authenticate] }, async (req: a
 // --- СТАРТ СЕРВЕРА ---
 const start = async () => {
   try {
-    await app.listen({ port: 3001, host: '0.0.0.0' });
-    console.log('🚀 API running on http://localhost:3001');
+    await app.listen({ port: 5000, host: '0.0.0.0' });
+    console.log('🚀 API running on http://localhost:5000');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
